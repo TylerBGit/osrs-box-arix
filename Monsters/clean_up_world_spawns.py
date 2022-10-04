c198d2c5aa23a1d6a13e7e3e04512d7dac4fed95
tyler <tylerbrownjones@gmail.com>
2022-10-04 23:55:55 +0100

    added python script to clean up world spawns

---
+++ Monsters/clean_up_world_spawns.py
@@ -0,0 +1,18 @@
+import json
+
+
+if __name__ == "__main__":
+    with open('world_spawns_old.json') as world_spawns:
+        data = json.load(world_spawns)
+
+        for d in data:
+            if 'colourReplacements' in d:
+                d.pop('colourReplacements')
+            if "chatHeadModels" in d:
+                d.pop('chatHeadModels')
+            if 'models' in d:
+                d.pop('models')
+
+        out_file_name = 'world_spawns_new.json'
+        with open(out_file_name, "w", newline="\n") as out_file:
+            json.dump(data, out_file, indent=4)
